856672e128bf478d58021c44d9c766a239df9296
svlandeg <sofie.vanlandeghem@gmail.com>
2021-07-02 17:14:44 +0200

    expand on actual code

--- spacy_legacy/architectures/parser.py
+++ spacy_legacy/architectures/parser.py
@@ -1,15 +1,18 @@
 from typing import Optional, List
 from thinc.types import Floats2d
-from spacy.util import registry
-from thinc.api import Model
+from thinc.api import Model, zero_init, use_ops
+
 from spacy.tokens import Doc
 from spacy.compat import Literal
+from spacy.errors import Errors
+from spacy.util import registry
 
-# TODO: replace with registered layer after spacy is released with the update
-from spacy.ml.models.parser import build_tb_parser_model
+# TODO: replace with registered layers after spacy is released with the update
+from spacy.ml._precomputable_affine import PrecomputableAffine
+from spacy.ml.tb_framework import TransitionModel
 
 
-@registry.architectures("spacy.TransitionBasedParser.v1")
+@registry.architectures.register("spacy.TransitionBasedParser.v1")
 def transition_parser_v1(
     tok2vec: Model[List[Doc], List[Floats2d]],
     state_type: Literal["parser", "ner"],
@@ -19,12 +22,30 @@ def transition_parser_v1(
     use_upper: bool = True,
     nO: Optional[int] = None,
 ) -> Model:
-    return build_tb_parser_model(
-        tok2vec,
-        state_type,
-        extra_state_tokens,
-        hidden_width,
-        maxout_pieces,
-        use_upper,
-        nO,
+
+    chain = registry.get("layers", "chain.v1")
+    list2array = registry.get("layers", "list2array.v1")
+    Linear = registry.get("layers", "Linear.v1")
+
+    if state_type == "parser":
+        nr_feature_tokens = 13 if extra_state_tokens else 8
+    elif state_type == "ner":
+        nr_feature_tokens = 6 if extra_state_tokens else 3
+    else:
+        raise ValueError(Errors.E917.format(value=state_type))
+    t2v_width = tok2vec.get_dim("nO") if tok2vec.has_dim("nO") else None
+    tok2vec = chain(tok2vec, list2array(), Linear(hidden_width, t2v_width))
+    tok2vec.set_dim("nO", hidden_width)
+    lower = PrecomputableAffine(
+        nO=hidden_width if use_upper else nO,
+        nF=nr_feature_tokens,
+        nI=tok2vec.get_dim("nO"),
+        nP=maxout_pieces,
     )
+    if use_upper:
+        with use_ops("numpy"):
+            # Initialize weights at zero, as it's a classification layer.
+            upper = Linear(nO=nO, init_W=zero_init)
+    else:
+        upper = None
+    return TransitionModel(tok2vec, lower, upper)
