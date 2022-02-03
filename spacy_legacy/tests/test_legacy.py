e25375cee2757af32f8d61690eb2776bd9fbb7fd
Daniël de Kok <me@danieldk.eu>
2022-02-03 12:44:10 +0100

    Add Tagger.v1

--- spacy_legacy/tests/test_legacy.py
+++ spacy_legacy/tests/test_legacy.py
@@ -8,6 +8,7 @@ FUNCTIONS = [
     ("architectures", "MaxoutWindowEncoder.v1"),
     ("architectures", "MishWindowEncoder.v1"),
     ("architectures", "MultiHashEmbed.v1"),
+    ("architectures", "Tagger.v1"),
     ("architectures", "TextCatBOW.v1"),
     ("architectures", "TextCatCNN.v1"),
     ("architectures", "TextCatEnsemble.v1"),
