50cd0c128c39cb6acb3b1c938215db59a11bd6b7
Daniël de Kok <me@danieldk.eu>
2023-12-08 20:00:22 +0100

    Revert "Update `spacy-legacy.TransitionBasedParser.v1` for parser refactor (#36)"
    This reverts commit a544ba3df6ea6cfe9c75ccc6b108977cd0231291.

--- spacy_legacy/architectures/parser.py
+++ spacy_legacy/architectures/parser.py
@@ -2,7 +2,6 @@ from typing import Optional, List, Literal
 from thinc.types import Floats2d
 from thinc.api import Model, zero_init, use_ops
 
-from spacy.ml.models.parser import build_tb_parser_model
 from spacy.tokens import Doc
 from spacy.errors import Errors
 from spacy.util import registry
@@ -17,15 +16,35 @@ def TransitionBasedParser_v1(
     use_upper: bool = True,
     nO: Optional[int] = None,
 ) -> Model:
-    # TODO: Add back use_upper once it is implemented in the refactored parser.
-    return build_tb_parser_model(
-        tok2vec,
-        state_type,
-        extra_state_tokens,
-        hidden_width,
-        maxout_pieces,
-        nO=nO,
+
+    chain = registry.get("layers", "chain.v1")
+    list2array = registry.get("layers", "list2array.v1")
+    Linear = registry.get("layers", "Linear.v1")
+    TransitionModel = registry.get("layers", "spacy.TransitionModel.v1")
+    PrecomputableAffine = registry.get("layers", "spacy.PrecomputableAffine.v1")
+
+    if state_type == "parser":
+        nr_feature_tokens = 13 if extra_state_tokens else 8
+    elif state_type == "ner":
+        nr_feature_tokens = 6 if extra_state_tokens else 3
+    else:
+        raise ValueError(Errors.E917.format(value=state_type))
+    t2v_width = tok2vec.get_dim("nO") if tok2vec.has_dim("nO") else None
+    tok2vec = chain(tok2vec, list2array(), Linear(hidden_width, t2v_width))
+    tok2vec.set_dim("nO", hidden_width)
+    lower = PrecomputableAffine(
+        nO=hidden_width if use_upper else nO,
+        nF=nr_feature_tokens,
+        nI=tok2vec.get_dim("nO"),
+        nP=maxout_pieces,
     )
+    if use_upper:
+        with use_ops("numpy"):
+            # Initialize weights at zero, as it's a classification layer.
+            upper = Linear(nO=nO, init_W=zero_init)
+    else:
+        upper = None
+    return TransitionModel(tok2vec, lower, upper, resize_output=resize_output_v1)
 
 
 def resize_output_v1(model, new_nO):
